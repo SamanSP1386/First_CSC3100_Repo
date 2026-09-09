9bd6993d9247d1144bccf03800d02ac1a860b300
saman <saman.sepehr86@gmail.com>
2026-09-08 22:48:05 -0700

    step 6 on IE1

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,13 +35,21 @@ const users = {
   ],
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -52,6 +60,22 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
